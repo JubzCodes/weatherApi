1bf01c62e6f95c6371d492c6a7fdf58b6befbb5b
JubzCodes <jadenbusiness@hotmail.comgit config --global user.name JubzCodesgit config --global user.email jadenbusiness@hotmail.com>
2022-08-02 21:06:17 -0400

    setup page

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -3,7 +3,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      HI
+      <div className="search">
+        <input />
+      </div>
+      <div className="container">
+        <div className="text">
+          <div className="location"></div>
+          <div className="date"></div>
+        </div>
+        <div className="widget">
+          <div className="temp"></div>
+          <div>
+            <div className='icon'></div>
+            <div className='description'></div>
+          </div>
+        </div>
+      </div>
+      <div className='extras'>
+        <div className='feels'></div>
+        <div className='humidity'></div>
+        <div className='wind'></div>
+      </div>
     </div>
   );
 }
